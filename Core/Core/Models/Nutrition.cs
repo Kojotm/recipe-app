3c159d61f92a188132e971180c6baed8a61eca91
Marcell Kajati <marcell.kajati@gmail.com>
2021-03-03 08:41:49 +0100

    Added model structure for recipes.

---
+++ Core/Core/Models/Nutrition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models
+{
+    public class Nutrition
+    {
+        public int Protein { get; set; }
+        public int Fat { get; set; }
+        public int Sugar { get; set; }
+        public int Salt { get; set; }
+        public int Carbohydrates { get; set; }
+    }
+}
